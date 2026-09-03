808eb9cf6ca28e0b5a7c005262316f1b30ead98a
LMH <90649822+LoneHilke@users.noreply.github.com>
2026-09-03 12:54:12 +0200

    Add Render error logging

--- play/settings.py
+++ play/settings.py
@@ -339,3 +339,24 @@ STORAGES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
