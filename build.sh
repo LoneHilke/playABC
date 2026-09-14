59739a0ebb5b158053b23ba86004e3ef49a2639f
LMH <90649822+LoneHilke@users.noreply.github.com>
2026-09-14 12:28:35 +0200

    Copy existing images to persistent disk

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 set -o errexit
 
 mkdir -p /opt/render/project/src/image
-
-if [ -d "/opt/render/project/src/image_seed" ]; then
-    cp -rn /opt/render/project/src/image_seed/. /opt/render/project/src/image/
-fi
+cp -rn /opt/render/project/src/image_seed/. /opt/render/project/src/image/
 
 python manage.py collectstatic --no-input
 python manage.py migrate
