d25857eb260eea64c9e6e8161421ba0df6274fa5
LMH <90649822+LoneHilke@users.noreply.github.com>
2026-09-14 12:19:12 +0200

    Seed persistent media disk

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+mkdir -p /opt/render/project/src/image
+
+if [ -d "/opt/render/project/src/image_seed" ]; then
+    cp -rn /opt/render/project/src/image_seed/. /opt/render/project/src/image/
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
